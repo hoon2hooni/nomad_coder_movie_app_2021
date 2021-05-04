188daf5df777a9b1957b0900e2ca699b5c39b62f
hoon2hooni <sanghoon.engineer@gmail.com>
2021-05-04 10:14:13 +0900

    Learn how to use props

--- src/App.js
+++ src/App.js
@@ -1,12 +1,49 @@
 import React from "react";
-import Potato from "./Potato";
+
+function Food({ name }){
+  return <h1> I like {name} </h1> ;
+}
+
+const foodILike = [
+  {
+    name: "kimchi",
+    flavor: "spicy"
+  },
+  {
+    name: "samgyupsal",
+    flavor: "spicy"
+  },
+  {
+    name: "kimchi",
+    flavor: "spicy"
+  },
+  {
+    name: "kimchi",
+    flavor: "spicy"
+  },
+  {
+    name: "kimchi",
+    flavor: "spicy"
+  }
+];
+
+//우리는 꼐속 reuse를 할수 있어
 
 function App() {
   return (
   <div>
-    HELLOO!!!!
-    <Potato />
-  </div> );
+    <h1>HELLOO!!!!</h1>
+
+    {/* name과 적음 */}
+    {/* food component에 prop name kimch를 줌 */} 
+    {/* prop  = property야 */}
+    {foodILike.map(dish => (
+      <Food name = {dish.name}/>
+      ))}
+    
+    
+
+  </div>);
 }
 
 export default App;
