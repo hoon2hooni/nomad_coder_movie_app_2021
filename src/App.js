c8cbfb6e1c1ead212b4460540f0ae9cb7297fb77
hoon2hooni <sanghoon.engineer@gmail.com>
2021-05-05 20:21:15 +0900

    Learn how to use setTimeout and componentDidMount function and method for using fetch data and change loading state

--- src/App.js
+++ src/App.js
@@ -3,44 +3,19 @@ import propTypes from 'prop-types';
 
 //필수과정 
 class App extends React.Component{
-  constructor(props){
-    super(props);
-    console.log("hello");
-  }
-  state ={
-    count: 0
+  state = {
+    isLoading: true
   };
-  add = () => {
-    this.setState(current => ({ count: current.count + 1}));
-  };
-  minus = () => {
-    this.setState(current => ({count: current.count - 1}));
-  };
-  //render 을 바꿔주어야 행
-  //setState를 하면 view가 다시 refresh됨
-  //setState를 안해주면 render function 다시 못불러ㅗㅇㅁ
-  //Mounting
-
-  //Updating : setState할때 사용됨
-  
-  //UnMounting
   componentDidMount(){
-    console.log("component rendered");
+    setTimeout(() =>{
+      this.setState({ isLoading: false });
+    }, 6000);
   }
-  componentDidUpdate(){
-    console.log("I just updated");
+  
+  render() { 
+    const { isLoading } = this.state;
+    return <div>{isLoading ? "Loading ...": "we are ready"}</div>;  
   }
-  componentWillMount(){
-    console.log("good bye components and see you");
-  }
-  render() {
-    console.log("render");
-    return (<div> 
-      <h1> The number is: {this.state.count}</h1>
-      <button onClick ={this.add}>Add</button>
-      <button onClick ={this.minus}>Minus</button>
-    </div>);
-  };
-};
+}
 
 export default App;
